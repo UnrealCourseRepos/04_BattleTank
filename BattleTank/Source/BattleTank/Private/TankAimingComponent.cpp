055f006f2bea76f7a6ed29855ea56f997e4dceff
Erp3000 <kastananderp@gmail.com>
2019-01-27 19:16:07 +0100

    Using FRotators in Unreal

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -18,50 +18,48 @@ void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent * BarrelToSet
 	Barrel = BarrelToSet;
 }
 
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
 	if (!Barrel) { return; }
 
 	FVector OutLaunchVelocity;
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+	bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity(
+		this,
+		OutLaunchVelocity,
+		StartLocation,
+		HitLocation,
+		LaunchSpeed,
+		false,
+		0,
+		0,
+		ESuggestProjVelocityTraceOption::DoNotTrace,
+		FCollisionResponseParams::DefaultResponseParam,
+		TArray < AActor *  >(),
+		false // Debug Line (RayCasting)
+	);
 
-	if (UGameplayStatics::SuggestProjectileVelocity(
-			this,
-			OutLaunchVelocity,
-			StartLocation,
-			HitLocation,
-			LaunchSpeed,
-			false,
-			0,
-			0,
-			ESuggestProjVelocityTraceOption::DoNotTrace,
-			FCollisionResponseParams::DefaultResponseParam,
-			TArray < AActor *  > (),
-			false // Debug Line (RayCasting)
-			)
-		)
-	{
-		auto TankName = GetOwner()->GetName();
-		auto AimVelocity = OutLaunchVelocity.GetSafeNormal();
-		UE_LOG(LogTemp, Warning, TEXT("Tank %s Aiming at %s"),*TankName ,*AimVelocity.ToString());
+	if (bHaveAimSolution) {
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+		MoveBarrelTowards(AimDirection);
 	}
 	// If no solution found do nothing
 }
 
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection) {
+	// Workout the diffrence between current barrel rotation and Aim Direction
+	auto TankName = GetOwner()->GetName();
+	auto BarrelRotation = Barrel->GetForwardVector().Rotation();
+	auto AimAsRotator = AimDirection.Rotation();
+	auto DeltaRotator = AimAsRotator; // - BarrelRotator;  // Delete this later when you'll declare this identifier
+
+	UE_LOG(LogTemp, Warning, TEXT("%s AimAsRotator %s"), *TankName , *DeltaRotator.ToString());
+
+
+	// Rotate barrel the right amount this frame
+	// Given a max elevation speed, and frame time
+
+	// so it will point at cordinates of where the player is looking over few frames
+	// Report back the rotation state of the barrel the corditantes of the hit and MAYBE if it hit any enemy tank
+
+}
