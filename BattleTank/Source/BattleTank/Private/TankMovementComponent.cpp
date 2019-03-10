47bfbf8cf7ed2657753c59d5f8dbd8be74c862f6
Erp3000 <kastananderp@gmail.com>
2019-03-10 17:52:24 +0100

    A Better Component Architecture

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,10 +2,20 @@
 
 #include "TankMovementComponent.h"
 #include "Engine/World.h"
+#include "TankTrack.h"
 
-
+void UTankMovementComponent::Initialise(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet) {
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 void UTankMovementComponent::IntendMoveForward(float Throw) {
 
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	//TODO prevent double speed due to double controll use
 
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward %f"),Throw);
+
+
 }
