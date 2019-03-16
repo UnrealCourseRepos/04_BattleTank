c782554719c6c78b1a4492f7b0c3971fed00381b
Erp3000 <kastananderp@gmail.com>
2019-03-16 12:49:43 +0100

    Finalising Class Code

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Martin Szabo
 
 #include "TankMovementComponent.h"
 #include "Engine/World.h"
@@ -29,8 +29,6 @@ void UTankMovementComponent::IntendMoveForward(float Throw) {
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-	//TODO prevent double speed due to double controll use
-
 }
 
 void UTankMovementComponent::IntendRotateClockwise(float Throw) {
@@ -38,6 +36,4 @@ void UTankMovementComponent::IntendRotateClockwise(float Throw) {
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-	//TODO prevent double speed due to double controll use
-
 }
